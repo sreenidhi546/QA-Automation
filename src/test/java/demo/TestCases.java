7978c33d5e392d821a5d6b4b542258ef1c7c3775
sreenidhi776 <sreenidhi776@gmail.com>
2024-12-15 12:16:12 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -9,6 +9,10 @@ import org.openqa.selenium.logging.LoggingPreferences;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
@@ -21,7 +25,44 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException{
+        Wrappers wrapper = new Wrappers();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+       driver.get("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+      Thread.sleep(2000);
+       WebElement nameInput = driver.findElement(By.xpath("(//input[@class='whsOnd zHQkBf'])[1]"));
+       wrapper.setText(nameInput, "Crio Learner");
 
+        WebElement practiceTextArea = driver.findElement(By.xpath("//textarea[@class='KHxj8b tL9Q4c']"));
+        wrapper.setText(practiceTextArea, wrapper.getCurrentEpochTime());
+
+        WebElement automationExperience = driver.findElement(By.xpath("(//div[@class='AB7Lab Id5V1'])[2]"));
+        wrapper.clickElement(automationExperience);
+        WebElement java = driver.findElement(By.xpath("(//div[@class='uHMk6b fsHoPb'])[1]"));
+        wrapper.clickElement(java);
+        WebElement selenium = driver.findElement(By.xpath("(//div[@class='uHMk6b fsHoPb'])[2]"));
+        wrapper.clickElement(selenium);
+        WebElement testNg = driver.findElement(By.xpath("(//div[@class='uHMk6b fsHoPb'])[4]"));
+         wrapper.clickElement(testNg);
+        WebElement salutationDropdown = driver.findElement(By.xpath("(//div[@jsname='d9BH4c'])[1]"));
+         wrapper.clickElement(salutationDropdown);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//span[text()='Mr'])[2]")));
+        WebElement dropDownValue = driver.findElement(By.xpath("(//span[text()='Mr'])[2]"));
+        wrapper.clickElement(dropDownValue);
+        WebElement date = driver.findElement(By.xpath("(//input[@class='whsOnd zHQkBf'])[2]"));
+        wrapper.setText(date, wrapper.getDateBeforeSevenDays());
+        WebElement hour = driver.findElement(By.xpath("(//input[@type='text'])[2]"));
+        wrapper.setText(hour, "07");
+        WebElement min = driver.findElement(By.xpath("(//input[@type='text'])[3]"));
+        wrapper.setText(min, "30");
+        WebElement btnSubmit = driver.findElement(By.xpath("(//div[@role='button'])[1]"));
+        wrapper.clickElement(btnSubmit);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='vHW8K']")));
+        WebElement thankYouText = driver.findElement(By.xpath("//div[@class='vHW8K']"));
+        System.out.println(thankYouText.getText());
+        
+    }
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -2,6 +2,7 @@ package demo.wrappers;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
@@ -16,4 +17,31 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public void setText(WebElement element, String text){
+        try{
+            element.clear();
+            element.sendKeys(text);
+        }catch(Exception e){
+            System.out.println("Not able to enter the text");
+        }
+    }
+
+    public static String getCurrentEpochTime(){
+          return "I want to be the best QA Engineer! "+Instant.now().toEpochMilli();
+        }
+
+    public void clickElement(WebElement element){
+        try{
+            element.click();
+        }catch(Exception e){
+            System.out.println("Not able to click on the element");
+        }
+    }
+
+     public static String getDateBeforeSevenDays(){
+            LocalDate currentDate = LocalDate.now();
+            LocalDate sevenDaysBefore = currentDate.minusDays(7);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            return sevenDaysBefore.format(formatter);
+        }
 }
